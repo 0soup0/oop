17068274de7b84abde74a3add28d8a523d50b553
0soup0 <117676942+0soup0@users.noreply.github.com>
2023-01-01 03:26:56 +0800

    Create class14.java

---
+++ ch12/class14.java
@@ -0,0 +1,9 @@
+package c12;
+
+public class class14 {
+    public static void main(String[] args) {
+        String str = "Habit is second nature.";
+        System.out.println("string = " + str);
+        System.out.println("substring = " + str.substring(16, 22));
+    }
+}

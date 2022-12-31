51266c05364a61c398f958637104584433722198
0soup0 <117676942+0soup0@users.noreply.github.com>
2023-01-01 03:26:11 +0800

    Create class09.java

---
+++ ch12/class09.java
@@ -0,0 +1,11 @@
+package c12;
+import c12.pack9.sub1.CSphere;
+import c12.pack9.sub2.CTrapezoid;
+public class class09 {
+    public static void main(String[] args) {
+        CSphere sp = new CSphere(2);
+        CTrapezoid tra = new CTrapezoid(2, 3, 4);
+        sp.show();
+        tra.show();
+    }
+}

1c675e860dc1c4f4c66ec444077ab9325055e1f2
0soup0 <117676942+0soup0@users.noreply.github.com>
2023-01-01 04:13:10 +0800

    Create class06.java

---
+++ ch15/class06.java
@@ -0,0 +1,21 @@
+package ch15;
+
+class Add implements Runnable {
+    private int n;
+    private int sum = 0;
+
+    public Add(int a) {
+        n = a;
+    }
+    public void run() {
+        sum = (1 + n) * n / 2;
+        System.out.println(sum);
+    }
+}
+public class class06 {
+    public static void main(String[] args) {
+        Add add = new Add(5);
+        Thread a1 = new Thread(add);
+        a1.start();
+    }
+}

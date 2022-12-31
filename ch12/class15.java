df1e8ac96f41c3d5e4d43fe22d626220eef6ef6d
0soup0 <117676942+0soup0@users.noreply.github.com>
2023-01-01 03:27:15 +0800

    Create class15.java

---
+++ ch12/class15.java
@@ -0,0 +1,9 @@
+package c12;
+
+public class class15 {
+    public static void main(String[] args) {
+        String str = Float.toString(123.45f);
+        System.out.println(str);
+        System.out.println("length = " + str.length());
+    }
+}

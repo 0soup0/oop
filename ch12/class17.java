52008342f1c4e48b7bca8142cb493c75cd76962f
0soup0 <117676942+0soup0@users.noreply.github.com>
2023-01-01 03:27:46 +0800

    Create class17.java

---
+++ ch12/class17.java
@@ -0,0 +1,9 @@
+package c12;
+
+public class class17 {
+    public static void main(String[] args) {
+        String str = "262904713";
+        short num = Short.parseShort(str);
+        System.out.printf("%d * 2 = %d\n", num, num * 2);
+    }
+}

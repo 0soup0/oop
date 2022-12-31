4cf2802ba3527f038049d97f02e4d3b6a5d5b8de
0soup0 <117676942+0soup0@users.noreply.github.com>
2023-01-01 03:27:31 +0800

    Create class16.java

---
+++ ch12/class16.java
@@ -0,0 +1,9 @@
+package c12;
+
+public class class16 {
+    public static void main(String[] args) {
+        String str = "262904713";
+        int num = Integer.parseInt(str);
+        System.out.printf("%d * 2 = %d\n", num, num * 2);
+    }
+}

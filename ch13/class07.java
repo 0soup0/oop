b99ad597011984de883d78709ec681cdab01060d
0soup0 <117676942+0soup0@users.noreply.github.com>
2023-01-01 03:40:31 +0800

    Create class07.java

---
+++ ch13/class07.java
@@ -0,0 +1,18 @@
+package ch13;
+
+public class class07 {
+    public static void test(int num, int den) throws ArithmeticException{
+        System.out.printf("%d / %d = %d", num, den, num / den);
+    }
+    public static void main(String[] args) {
+        try{
+            test(5, 0);
+        }
+        catch(ArithmeticException e){
+            System.out.println("除數為 0");
+        }
+        finally{
+            System.out.println("end of main() method!!");
+        }
+    }
+}

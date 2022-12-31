58fc25363c2e4307fa83b9b41fe6339bda64a1ea
0soup0 <117676942+0soup0@users.noreply.github.com>
2023-01-01 03:25:47 +0800

    Create class08.java

---
+++ ch12/class08.java
@@ -0,0 +1,13 @@
+package c12;
+import c12.pack6.subpack1.Ccircle;
+import c12.pack6.subpack2.CRectangle;
+import c12.pack6.subpack1.subsubpack.CStyle;
+public class class08 {
+    public static void main(String[] args) {
+        Ccircle cir = new Ccircle();
+        CRectangle rect = new CRectangle();
+        CStyle sty = new CStyle(1);
+        cir.show();
+        rect.show();
+    }
+}
